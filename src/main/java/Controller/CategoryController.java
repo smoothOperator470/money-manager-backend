6faf1a73c77c25bf1ad6c6543c0b0f81d0187f75
Aditya Krishnan <aditya.krishnan389@gmail.com>
2026-07-27 23:06:56 +0530

    Changes in controller code

--- src/main/java/Controller/CategoryController.java
+++ src/main/java/Controller/CategoryController.java
@@ -1,5 +1,48 @@
 package Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import Service.CategoryService;
+import dto.CategoryDTO;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+
 public class CategoryController {
-    
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategory = categoryService.saveCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getCategories() {
+        List<CategoryDTO> categories = categoryService.getCategoriesForCurrentUser();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<List<CategoryDTO>> getCategoriesByTypeForCurrentUser(@PathVariable String type) {
+        List<CategoryDTO> list = categoryService.getCategoriesByTypeForCurrentUser(type);
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long categoryId,
+                                                    @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO updatedCategory = categoryService.updateCategory(categoryId, categoryDTO);
+        return ResponseEntity.ok(updatedCategory);
+    }
 }
--- src/main/java/Controller/DashboardController.java
+++ src/main/java/Controller/DashboardController.java
@@ -1,5 +1,24 @@
 package Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
 public class DashboardController {
-    
+        private final DashboardService dashboardService;
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getDashboardData() {
+        Map<String, Object> dashboardData = dashboardService.getDashboardData();
+        return ResponseEntity.ok(dashboardData);
+    }
 }
